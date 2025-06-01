b93c267484e815080c02e1f2a36f8c09e5e36b4d
MeGaLoDoN <megalodon2287@gmail.com>
2025-06-01 14:55:52 -0700

    Input WIP

--- utils.asm
+++ utils.asm
@@ -35,6 +35,43 @@ Memset: ; hl: destination; de: destination end; c: value to set
 	cp d
 	jr nz, Memset
 	ret
+UpdateInputNibble: ; a: input group select
+  	ldh [rP1], a
+  	call .knownret 
+  	ldh a, [rP1] 
+  	ldh a, [rP1]
+  	ldh a, [rP1] 
+  	or $F0 
+.knownret
+ 	ret
+
+MACRO UPDATE_INPUT
+	ldh a, [KEY_STATE]
+	ldh [PREV_KEY_STATE], a
+
+  	ld a, P1F_GET_BTN
+  	call UpdateInputNibble
+  	ld l, a 
+
+  	ld a, P1F_GET_DPAD
+  	call UpdateInputNibble
+  	swap a
+  	xor l 
+	ldh [KEY_STATE], a
+
+  	ld a, P1F_GET_NONE
+  	ldh [rP1], a
+ENDM
+
+MACRO CHECK_KEY_PRESS ; \1: key, \2: function to call
+	ldh a, [KEY_STATE]
+	bit \1, a
+	jr z, .noPress\@
+	ldh a, [PREV_KEY_STATE]
+	bit \1, a
+	call z, \2
+.noPress\@:
+ENDM
 
 MACRO START_GDMA ; \1: destination; \2: source; \3: number of bytes
 	ld a, HIGH(\2)

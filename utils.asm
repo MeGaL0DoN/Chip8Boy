f1d43ad3e117e7828c28c9fa42c59c8112a84228
MeGaLoDoN <megalodon2287@gmail.com>
2025-06-03 21:14:26 -0700

    fully implemented input, and few other things

--- utils.asm
+++ utils.asm
@@ -35,6 +35,49 @@ Memset: ; hl: destination; de: destination end; c: value to set
 	cp d
 	jr nz, Memset
 	ret
+
+ConvertToBCD3: ; hl -> bc
+	ld bc, $FF64 ; b = -1, c = 100
+.countHundreds:
+	inc b
+	ld a, l
+	sub c
+	ld l, a
+	ld a, h
+	sbc 0
+	ld h, a
+	jr nc, .countHundreds
+	ld a, l
+	add c
+	ld c, -1
+.countTenths:
+	inc c
+	sub 10
+	jr nc, .countTenths
+	swap c
+	add 10
+	or c
+	ld c, a
+	ret
+
+Mul8x8: ; b x c -> hl
+	ld hl, 0
+	ld a, b
+	and a
+	ret z
+	ld d, 0
+	ld e, b
+.loop
+	bit 0, c
+	jr z, .skipAdd
+	add hl, de
+.skipAdd
+	sla e
+	rl d
+	srl c
+	jr nz, .loop
+	ret
+	
 UpdateKeys:
 	MACRO readNibble
 		ldh [rP1], a
@@ -48,14 +91,18 @@ UpdateKeys:
 	readNibble()
   	ld l, a 
 
+	ld a, P1F_GET_DPAD
 	readNibble()
   	swap a
   	xor l 
 	ld l, a
 	ldh a, [KEY_STATE]
 	xor l
+	ld h, a
 	and l
 	ldh [PRESSED_KEYS], a
+	xor h
+	ldh [RELEASED_KEYS], a
 	ld a, l
 	ldh [KEY_STATE], a
 
